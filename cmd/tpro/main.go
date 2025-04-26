d699c269ce25f55c4c1b2222ed9905ed3158c197
Pavel Erokhin <erokhin.p.n@citilink.ru>
2025-04-26 18:52:11 +0400

    added custom dns to regular proxy

--- cmd/tpro/main.go
+++ cmd/tpro/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env.example file")
 	}
 
 	proxyType := os.Getenv("TYPE")
